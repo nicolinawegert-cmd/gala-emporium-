0df8296cc9aa7d6af14ea6d732b5337b3d9a6a2e
Nicolina Wegert <nicolinawegert@gmail.com>
2025-11-10 10:34:36 +0100

    Uncomment rally-house import and menu entry

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import loadBooking from './booking.js';
 import loadPulseRoom from './pages/the-pulse-room.js';
 import loadJazzCorner from './pages/jazz-corner.js';
 import loadGiggleGalaxy from './pages/giggle-galaxy.js';
-//import loadRallyHouse from './pages/rally-house.js';
+import loadRallyHouse from './pages/rally-house.js';
 import loadAdmin from './admin.js';
 
 
@@ -14,10 +14,10 @@ const menu = {
   "start": { label: 'Start', function: loadStart },
   "jazz-corner": { label: 'Jazz-corner', function: loadJazzCorner },
   "giggle-galaxy": { label: 'Giggle-galaxy', function: loadGiggleGalaxy },
-  //"rally-house": { label: 'Rally-house', function: loadRallyHouse },
+  "rally-house": { label: 'Rally-house', function: loadRallyHouse },
   "the-pulse-room": { label: 'The-pulse-room', function: loadPulseRoom },
   "booking": { label: 'Booking', function: loadBooking },
-  "admin": {label: 'Admin', function: loadAdmin },
+  "admin": { label: 'Admin', function: loadAdmin },
 };
 
 function createMenu() { //funktion som skapar menyn och returnerar en html sträng object.entries gör om meny till en array .map går igenom varje element i arrayen och skapar en länk för varje meny objekt.
